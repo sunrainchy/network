620f02ea34215f57e20666e05595f9c0110e42a9
chy <1498752974@qq.com>
2014-05-27 13:05:19 +0800

    add select_client.c

---
+++ select_client.c
@@ -0,0 +1,24 @@
+#include <sys/socket.h>
+#include <unistd.h>
+#include <linux/in.h>
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+char buf[10000];
+int main(){
+	int connfd,sock_fd;
+	struct sockaddr_in client,info;
+	socklen_t len;
+	sock_fd=socket(AF_INET,SOCK_STREAM,0);
+	bzero(&client,sizeof(client));
+	client.sin_family=AF_INET;
+	client.sin_port=htons(10222);
+	client.sin_addr.s_addr=inet_addr("192.168.0.15");
+	if(connect(sock_fd,(struct sockaddr*)&client,sizeof(client))<0)printf("connect error\n");
+	else printf("success\n");
+	while(scanf("%s",buf)!=EOF){
+		write(sock_fd,buf,strlen(buf));
+	}
+	return 0;
+}
+
