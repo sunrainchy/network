cdd224296a6fa1eb6505f34408e75c17d3d4114d
chy <1498752974@qq.com>
2014-06-01 20:41:18 +0800

    nothint

--- udpclient.c
+++ udpclient.c
@@ -10,6 +10,7 @@ char str[maxn];
 int main(){
 	int udp_sock_fd,len;
 	struct sockaddr_in client,server;
+	bzero(&client,sizeof(client));
 	client.sin_family=AF_INET;
 	client.sin_port=htons(SERVER_PORT);
 	client.sin_addr.s_addr=inet_addr("192.168.0.15");
@@ -20,8 +21,11 @@ int main(){
 	}
 	strcpy(str,"hello world\n");
 	int t=1024,num=0;
+	connect(udp_sock_fd,(struct sockaddr*)&client,sizeof(client));
 	while(t--){
-		sendto(udp_sock_fd,str,strlen(str)+1,0,(struct sockaddr*)&client,sizeof(client));
+		write(udp_sock_fd,str,sizeof(str));
+		read(udp_sock_fd,str,sizeof(str));
 	}	
 	return 0;
 }
+
